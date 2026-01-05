51bcca66783d040644e7f8e9c9a8b29f27f682d5
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-04 23:56:17 -0500

    added test_subtract() function

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,3 +5,8 @@ from calculator import Calculator
 def test_add():
     c = Calculator()
     assert c.add(2, 3).value == 5
+
+
+def test_subtract():
+    c = Calculator()
+    assert c.subtract(10, 4).value == 6
